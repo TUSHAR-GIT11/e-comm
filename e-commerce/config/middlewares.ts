d15c402fc82d8ff8d13ca8cdd1b064c00ae654ee
TUSHAR-GIT11 <tushargupta10th@gmail.com>
2026-06-28 21:38:09 +0530

    safe

--- e-commerce/config/middlewares.ts
+++ e-commerce/config/middlewares.ts
@@ -2,7 +2,18 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'https://e-comm-blond-nine.vercel.app',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
