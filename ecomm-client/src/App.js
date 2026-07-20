384ff652376ef692958348efdaee3d1437c92de9
TUSHAR-GIT11 <tushargupta10th@gmail.com>
2026-07-20 22:45:27 +0530

    fix: hide navbar and categories on login/signup pages

--- ecomm-client/src/App.js
+++ ecomm-client/src/App.js
@@ -1,6 +1,6 @@
 import Navbar from "./components/navbar";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Signup from "./pages/signup";
 import Login from "./pages/login";
 import Product from "./pages/product";
@@ -9,25 +9,36 @@ import { CartProvider } from "react-use-cart"
 import Categories from "./components/categories";
 import ProductByCategory from "./pages/productByCategory";
 import ProtectedRoute from "./components/protectedRoute";
-function App() {
+
+function AppContent() {
+  const location = useLocation();
+  const hideNavbar = location.pathname === "/login" || location.pathname === "/signup";
+
   return (
-    
-      <CartProvider idAttribute="id"  >
-        <BrowserRouter>
-        <Navbar />
-        <Categories/>
-        <Routes>
-         
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-           <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          <Route path="/product/:pid" element={<ProtectedRoute><Product /></ProtectedRoute>} />
-          <Route path="/cart" element={<ProtectedRoute><Cart/></ProtectedRoute>}/>
-          <Route path="/category/:cid" element={<ProtectedRoute><ProductByCategory/></ProtectedRoute>}/>
-        </Routes>
-      </BrowserRouter>
-      </CartProvider>
-    
+    <>
+      {!hideNavbar && <Navbar />}
+      {!hideNavbar && <Categories />}
+      <Routes>
+       
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+         <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/product/:pid" element={<ProtectedRoute><Product /></ProtectedRoute>} />
+        <Route path="/cart" element={<ProtectedRoute><Cart/></ProtectedRoute>}/>
+        <Route path="/category/:cid" element={<ProtectedRoute><ProductByCategory/></ProtectedRoute>}/>
+      </Routes>
+    </>
   );
 }
+
+function App() {
+  return (
+    <CartProvider idAttribute="id">
+      <BrowserRouter>
+        <AppContent />
+      </BrowserRouter>
+    </CartProvider>
+  );
+}
+
 export default App;
